e3b9348e7f86e0408319f654f1af06da583675f2
Piyush kumar <piyush2004.dev@gmail.com>
2025-06-30 21:27:59 +0530

    add vercel.json

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const main = require('./config/database');
-require('dotenv').config();
-const authRoutre = require('./routes/userAuth');
-const cookieParser = require('cookie-parser');
-const redisClint = require('./config/redis');
-const problemRouter = require('./routes/problemCreator');
-const submitRouter = require('./routes/submit');
-const cors = require('cors');
-const aiRouter = require('./routes/aiChatting');
-const noteRouter = require('./routes/noteSection')
+const main = require("./config/database");
+require("dotenv").config();
+const authRoutre = require("./routes/userAuth");
+const cookieParser = require("cookie-parser");
+const redisClint = require("./config/redis");
+const problemRouter = require("./routes/problemCreator");
+const submitRouter = require("./routes/submit");
+const cors = require("cors");
+const aiRouter = require("./routes/aiChatting");
+const noteRouter = require("./routes/noteSection");
 
-
-app.use(cors({
-    origin: ['http://localhost:5173', "http://localhost:5174"],
-    credentials: true 
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser()); // Middleware to parse cookies
 
+app.use("/auth", authRoutre); // user  login,registration,logout and progile view code
+app.use("/problem", problemRouter);
+app.use("/submit", submitRouter);
+app.use("/ai", aiRouter);
+app.use("/note", noteRouter);
 
-app.use('/auth' , authRoutre) // user  login,registration,logout and progile view code
-app.use('/problem', problemRouter)
-app.use("/submit",submitRouter )
-app.use("/ai" , aiRouter)
-app.use("/note" , noteRouter)
+const surver = async () => {
+  try {
+    await Promise.all([main(), redisClint.connect()]);
 
+    console.log("Connected to MongoDB and Redis");
 
-
-const surver = async ()=>{
-    
-    try{
-         await Promise.all([main(),redisClint.connect()]);
-
-        console.log('Connected to MongoDB and Redis');
-
-         app.listen(5500, () => {
-        console.log('Server is running on port 5500');
+    app.listen(5500, () => {
+      console.log("Server is running on port 5500");
     });
-    }
-    catch(err){
-        console.error('Error connecting to MongoDB:', err);
-    }
-}
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
+};
 surver();
-
+module.exports = app;
 
 // main()
 // .then(async () => {
@@ -54,6 +50,5 @@ surver();
 //     app.listen(5500, () => {
 //         console.log('Server is running on port 5500');
 //     });
-    
-// })
 
+// })
