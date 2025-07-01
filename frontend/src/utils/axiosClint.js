df1edf34441904880dc2450654e64fdcbf7fb2a9
adivdeco <sadiv120@gmail.com>
2025-07-01 23:20:55 +0530

    feat: Update API_URL in environment variables and configure axios client to use it

--- frontend/src/utils/axiosClint.js
+++ frontend/src/utils/axiosClint.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'http://localhost:5500',
+    baseURL: process.env.API_URL || 'http://localhost:5500',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
